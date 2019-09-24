87041b8b93b0533b67de8992701c7aa385046836
kelly r maass <kmaass77627@gmail.com>
2019-09-24 01:02:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,12 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
+
+def link 
+  p "It's dangerous "
